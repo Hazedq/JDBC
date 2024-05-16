f9c6f49c53bf9aa44eaff8664791b27df35087f0
Anikin Denis <denis4nikin@yandex.ru>
2024-05-16 21:54:22 +0400

    первая фиксация

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,34 +2,93 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
+import static jm.task.core.jdbc.util.Util.getConnection;
+
 public class UserDaoJDBCImpl implements UserDao {
+
+    private final Connection connection = getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
 
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(" CREATE TABLE IF NOT EXISTS Users (\n" +
+                    " id BIGINT NOT NULL AUTO_INCREMENT,\n" +
+                    " name varchar(25),\n" +
+                    " surname varchar(25),\n" +
+                    " age DOUBLE,\n" +
+                    " PRIMARY KEY (id)\n" +
+                    " ); ");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 
     public void dropUsersTable() {
 
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS Users;");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Users (name, surname, age) VALUES (?, ?, ?)")) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println("User с именем — " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
 
+        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Users WHERE id = ?")) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> allusers = new ArrayList<>();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Users")) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("surname"));
+                user.setAge(resultSet.getByte("age"));
+                allusers.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return allusers;
     }
 
     public void cleanUsersTable() {
 
+        try (PreparedStatement preparedStatement = connection.prepareStatement("TRUNCATE TABLE Users;")) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://127.0.0.1:3306/test_jdbc";
+    private final static String USERNAME = "jpauser";
+    private final static String PASSWORD = "jpapwd";
+
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Соединение установлено!");
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("Ошибка. Соединение не установлено.");
+            return null;
+        }
+
+    }
+
+
 }
